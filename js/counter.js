08c3921043004cef34633efe630bc97ee3df54f9
salema1499 <salmakhaled1999iti@gmail.com>
2023-10-07 13:53:42 +0200

    first commit

--- js/counter.js
+++ js/counter.js
@@ -1,15 +1,42 @@
-let valueDisplays = document.querySelectorAll(".num");
-let section=document.querySelector(".counter")
+// let valueDisplays = document.querySelectorAll(".num");
+// let section=document.querySelector(".counter")
+
+// function startcount(el){
+//     let goal=el.dataset.goal;
+//     let count =setInterval(()=>{
+//         el.textContent++;
+//         if(el.textContent==goal){
+//             clearInterval(count)
+//         }
+//     },10)
+// }
+
+
+// startcount(document.querySelectorAll(".num"))
+
+
+
+
+let nums=document.querySelectorAll(".nums .num");
+let section=document.querySelector(".three")
+let started=false;
+
+
+window.onscroll=function(){
+    if(window.scrollY>=section.offsetTop){
+        if(!started){
+            nums.forEach((num)=>startcount(num));
+        }
+        started=true;
+    }
+};
 
 function startcount(el){
     let goal=el.dataset.goal;
     let count =setInterval(()=>{
         el.textContent++;
-        if(el.textContent==goal){
+        if(el.textContent == goal){
             clearInterval(count)
         }
-    },10)
+    },2000 / goal)
 }
-
-
-startcount(document.querySelectorAll(".num"))
